36678ff69d0df1e06c0bc73412bb1c9fb3125022
Brandon Fraune <brandon.fraune@gmail.com>
2023-06-28 22:12:36 -0500

    Initialize iOS Project

---
+++ SwiftUI-Fonts/SwiftUI_FontsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FontsApp.swift
+//  SwiftUI-Fonts
+//
+//  Created by Brandon Fraune on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FontsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
